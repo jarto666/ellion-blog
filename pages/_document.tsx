b6b245185b752707f50dd52b611166836b83c36c
Ilya Livshits <jarto666@gmail.com>
2025-01-08 09:06:30 +0100

    Favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,48 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => {
   const setInitialTheme = `
     (function() {
-      const theme = localStorage.getItem('theme') || 'light';
+      const theme = localStorage.getItem('theme') || 'dark';
       document.documentElement.setAttribute('data-theme', theme);
     })();
   `;
 
   return (
     <Html>
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/icons/favicon.ico" />
+
+        {/* Additional Favicons */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+
+        {/* Android Chrome Icons */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icons/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/icons/android-chrome-512x512.png"
+        />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+      </Head>
       <body>
         <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
         <Main />
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,9 +1,10 @@
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps, Metadata } from "next";
 import { fetchBlogPosts } from "../../lib/storyblok";
 import { BlogPost } from "../../lib/storyblok";
 import Image from "next/image";
 import BlogPostContent from "../../components/blog/blogPostContent";
 import { formatDate } from "../../lib/utils";
+import Head from "next/head";
 
 interface BlogPostPageProps {
   post: BlogPost;
@@ -11,30 +12,44 @@ interface BlogPostPageProps {
 
 const BlogPostPage: React.FC<BlogPostPageProps> = ({ post }) => {
   return (
-    <article className="max-w-screen-lg mx-auto w-full p-4 bg-background text-foreground">
-      <div className="max-w-screen-md mt-8 mb-8">
-        <p className="text-sm text-foreground-300 mb-2 underline">
-          Published on: {formatDate(post.publishedDate)}
-        </p>
-        <h1 className="text-3xl font-semibold text-foreground">{post.title}</h1>
-      </div>
-
-      {post.image && (
-        <div className="relative h-auto mb-8">
-          <Image
-            src={post.image.file.url}
-            alt={post.title}
-            width={1920}
-            height={1280}
-            className="rounded-lg shadow-lg"
-          />
+    <>
+      <Head>
+        <title>{post.title} | Ellion Blog</title>
+        <meta name="description" content={post.description} />
+        <meta property="og:title" content={post.title} />
+        <meta property="og:description" content={post.description} />
+        {post.image && (
+          <meta property="og:image" content={post.image.file.url} />
+        )}
+        <meta property="og:type" content="article" />
+      </Head>
+      <article className="max-w-screen-lg mx-auto w-full p-4 bg-background text-foreground">
+        <div className="max-w-screen-md mt-8 mb-8">
+          <p className="text-sm text-foreground-300 mb-2 underline">
+            Published on: {formatDate(post.publishedDate)}
+          </p>
+          <h1 className="text-3xl font-semibold text-foreground">
+            {post.title}
+          </h1>
         </div>
-      )}
 
-      <div className="max-w-screen-md text-lg text-foreground-200 justified-text">
-        <BlogPostContent content={post.body}></BlogPostContent>
-      </div>
-    </article>
+        {post.image && (
+          <div className="relative h-auto mb-8">
+            <Image
+              src={post.image.file.url}
+              alt={post.title}
+              width={1920}
+              height={1280}
+              className="rounded-lg shadow-lg"
+            />
+          </div>
+        )}
+
+        <div className="max-w-screen-md text-lg text-foreground-200 justified-text">
+          <BlogPostContent content={post.body}></BlogPostContent>
+        </div>
+      </article>
+    </>
   );
 };
 
@@ -55,6 +70,16 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return { notFound: true };
   }
   return { props: { post } };
+};
+
+// export const metadata = ({ post }: { post: BlogPost }) => ({
+//   title: post.title,
+//   description: post.description || "Read this amazing blog post on our site.",
+// });
+
+export const metadata: Metadata = {
+  title: "metatitle",
+  description: "metadescription",
 };
 
 export default BlogPostPage;
